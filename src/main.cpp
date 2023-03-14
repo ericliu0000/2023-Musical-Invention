42db172bc0d1b01d2e11c5e7bb58c1b06207d13c
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2023-03-14 14:49:56 -0400

    add multistepper for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
+#include <MultiStepper.h>
 
 const int MICROSTEPPING = 16;
 const float STEPS_PER_R = 200.0 * MICROSTEPPING;
@@ -27,18 +28,77 @@ const int E_STEP_PIN = 26;
 const int E_DIR_PIN = 28;
 const int E_ENABLE_PIN = 24;
 
+long positions[10][4] = {
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+};
+
 AccelStepper stepper1 = AccelStepper(AccelStepper::DRIVER, X_STEP_PIN, X_DIR_PIN);
+// AccelStepper stepper2 = AccelStepper(AccelStepper::DRIVER, Y_STEP_PIN, Y_DIR_PIN);
+// AccelStepper stepper3 = AccelStepper(AccelStepper::DRIVER, Z_STEP_PIN, Z_DIR_PIN);
+// AccelStepper stepper4 = AccelStepper(AccelStepper::DRIVER, E_STEP_PIN, E_DIR_PIN);
+
+// MultiStepper steppers;
 
 void setup()
 {
+    // Always enable stepper
+    pinMode(X_ENABLE_PIN, OUTPUT);
+    pinMode(Y_ENABLE_PIN, OUTPUT);
+    pinMode(Z_ENABLE_PIN, OUTPUT);
+    pinMode(E_ENABLE_PIN, OUTPUT);
+
+    digitalWrite(X_ENABLE_PIN, 0);
+    digitalWrite(Y_ENABLE_PIN, 0);
+    digitalWrite(Z_ENABLE_PIN, 0);
+    digitalWrite(E_ENABLE_PIN, 0);
+
+    // Configure motors
     stepper1.setMaxSpeed(SPEED);
+    stepper1.setSpeed(SPEED);
     stepper1.setAcceleration(ACCELERATION);
+
+    /*
+    stepper2.setMaxSpeed(SPEED);
+    stepper2.setSpeed(SPEED);
+    stepper2.setAcceleration(ACCELERATION);
+
+    stepper3.setMaxSpeed(SPEED);
+    stepper3.setSpeed(SPEED);
+    stepper3.setAcceleration(ACCELERATION);
+
+    stepper4.setMaxSpeed(SPEED);
+    stepper4.setSpeed(SPEED);
+    stepper4.setAcceleration(ACCELERATION);
+
+    // Set positions
+    stepper1.moveTo(10 * STEPS_PER_R);
+    stepper2.moveTo(10 * STEPS_PER_R);
+    stepper3.moveTo(10 * STEPS_PER_R);
+    stepper4.moveTo(10 * STEPS_PER_R);
+    */
 }
 
 void loop()
 {
-   stepper1.move(2 * STEPS_PER_R);
-   stepper1.runToPosition();
-   delay(1000);
+    stepper1.move(2 * STEPS_PER_R);
+    stepper1.runToPosition();
+    delay(1000);
+    stepper1.move(-2 * STEPS_PER_R);
+    stepper1.runToPosition();
+    delay(1000);
 
+    // for (auto& position : positions) {
+    //     steppers.moveTo(position);
+    //     steppers.runSpeedToPosition();
+    //     delay(100);
+    // }
 }
